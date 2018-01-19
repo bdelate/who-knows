dc6d5d2f4c3830bea38bf54cf17686ddc4d0ab7b
bdelate <brendon.delate@gmail.com>
2018-01-19 04:43:14 -0500

    Implemented forms/views/templates for questions to require tags. Updated existing question tests to work with tags. Implemented new tests related to tags.

--- whoknows/questions/tests/test_views.py
+++ whoknows/questions/tests/test_views.py
@@ -2,15 +2,27 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.contrib import auth
-from questions.models import Question
+from questions.models import Question, Tag
 
 
-class QuestionCreateAndDetailTest(TestCase):
+class BaseTestMixins:
+
+    def login_and_create_question(self):
+        self.client.post(reverse('account:login'), self.credentials)
+        response = self.client.post(reverse('questions:create'),
+                                    data={'title': 'test title', 'content': 'test content', 'tags': self.tag1.id},
+                                    follow=True)
+        return response
+
+
+class QuestionCreateAndDetailTest(TestCase, BaseTestMixins):
 
     def setUp(self):
         User = get_user_model()
         self.credentials = {'username': 'john', 'password': 'p@ssw0rd'}
         self.user = User.objects.create_user(**self.credentials)
+        self.tag1 = Tag.objects.create(name='tag1')
+        self.tag2 = Tag.objects.create(name='tag2')
 
     def test_logged_out_user_cannot_ask_question(self):
         user = auth.get_user(self.client)
@@ -21,14 +33,12 @@ class QuestionCreateAndDetailTest(TestCase):
         self.assertTemplateUsed(response, 'account/login.html')
 
     def test_create_question_redirects_to_detail(self):
-        self.client.post(reverse('account:login'), self.credentials)
-        response = self.client.post(reverse('questions:create'), data={'title': 'test title', 'content': 'test content'}, follow=True)
+        response = self.login_and_create_question()
         self.assertRedirects(response, reverse('questions:detail', args=['test-title']), status_code=302)
         self.assertTemplateUsed(response, 'questions/detail.html')
 
     def test_logged_out_user_can_access_question_detail(self):
-        self.client.post(reverse('account:login'), self.credentials)
-        self.client.post(reverse('questions:create'), data={'title': 'test title', 'content': 'test content'})
+        response = self.login_and_create_question()
         self.client.get(reverse('account:logout'))
         user = auth.get_user(self.client)
         self.assertEqual(user.is_authenticated, False)
@@ -40,10 +50,26 @@ class QuestionCreateAndDetailTest(TestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_cannot_create_duplicate_title(self):
-        self.client.post(reverse('account:login'), self.credentials)
-        self.client.post(reverse('questions:create'), data={'title': 'test title', 'content': 'test content'})
-        response = self.client.post(reverse('questions:create'), data={'title': 'test title', 'content': 'test content'})
+        self.login_and_create_question()
+        response = self.client.post(reverse('questions:create'),
+                                    data={'title': 'test title', 'content': 'test content', 'tags': 1},
+                                    follow=True)
         self.assertContains(response, 'Question with this Title already exists.')
+
+    def test_create_question_with_multiple_tags(self):
+        self.client.post(reverse('account:login'), self.credentials)
+        response = self.client.post(reverse('questions:create'),
+                                    data={'title': 'test title', 'content': 'test content', 'tags': [self.tag1.id, self.tag2.id]},
+                                    follow=True)
+        self.assertContains(response, 'tag1')
+        self.assertContains(response, 'tag2')
+
+    def test_question_must_have_a_tag(self):
+        self.client.post(reverse('account:login'), self.credentials)
+        response = self.client.post(reverse('questions:create'),
+                                    data={'title': 'test title', 'content': 'test content'},
+                                    follow=True)
+        self.assertContains(response, 'This field is required')
 
 
 class QuestionHomePageTest(TestCase):
--- whoknows/questions/views.py
+++ whoknows/questions/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, DetailView, ListView
-from .models import Question
+from django.views.generic import TemplateView, DetailView, ListView
+from .models import Question, Tag
+from .forms import QuestionForm, TagForm
+from django.shortcuts import redirect, render
 
 
 class HomePage(ListView):
@@ -12,15 +14,29 @@ class HomePage(ListView):
     paginate_by = 3
 
 
-class QuestionCreate(LoginRequiredMixin, CreateView):
+class QuestionCreate(LoginRequiredMixin, TemplateView):
 
-    model = Question
     template_name = 'questions/create.html'
-    fields = ['title', 'content']
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
+    def get_context_data(self, **kwargs):
+        return {'question_form': QuestionForm(),
+                'tag_form': TagForm()}
+
+    def post(self, request, *args, **kwargs):
+        question_form = QuestionForm(request.POST)
+        tag_form = TagForm(request.POST)
+        if question_form.is_valid() and tag_form.is_valid():
+            question_form.instance.user = self.request.user
+            question = question_form.save()
+            tags = tag_form.cleaned_data['tags']
+            for tag_id in tags:
+                obj, created = Tag.objects.get_or_create(id=tag_id)
+                question.tags.add(obj)
+            return redirect(question)
+        return render(request,
+                      self.template_name,
+                      {'question_form': question_form,
+                       'tag_form': tag_form})
 
 
 class QuestionDetail(DetailView):
