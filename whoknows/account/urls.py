c6144f01bbf7ff16f8ccfb53859793fe1b20f724
bdelate <brendon.delate@gmail.com>
2018-01-26 04:46:08 -0500

    Added users latest questions to profile view along with a link to view all their questions

--- whoknows/account/urls.py
+++ whoknows/account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import AccountCreate, ProfileUpdate
+from .views import UserQuestionList, AccountCreate, ProfileUpdate
 from django.contrib.auth import views as auth_views
 
 app_name = 'account'
 
 urlpatterns = [
     path('', ProfileUpdate.as_view(), name='profile'),
+    path('questions', UserQuestionList.as_view(), name='user_questions'),
     path('create/', AccountCreate.as_view(), name='create'),
     path("login/", auth_views.LoginView.as_view(template_name="account/login.html"), name='login'),
     path("logout/", auth_views.LogoutView.as_view(), name='logout'),
