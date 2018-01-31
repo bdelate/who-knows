e2cb5e048aa3b639172725159eecd06270486f42
bdelate <brendon.delate@gmail.com>
2018-01-31 03:04:34 -0500

    Created form/view/model tests for comment functionality. CreateComment view changed to check for a valid form and question before doing the logged in user check

--- whoknows/comments/views.py
+++ whoknows/comments/views.py
@@ -9,22 +9,22 @@ from django.urls import reverse
 class CreateComment(View):
 
     def post(self, request, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            comment_form = CommentForm(request.POST)
-            if comment_form.is_valid():
-                object_id = comment_form.cleaned_data['object_id']
-                if comment_form.cleaned_data['comment_type'] == 'question':
-                    try:
-                        object_instance = Question.objects.get(id=object_id)
-                    except ObjectDoesNotExist:
-                        return JsonResponse({'response': 'Invalid Question', 'type': 'comment'}, status=400)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            object_id = comment_form.cleaned_data['object_id']
+            if comment_form.cleaned_data['comment_type'] == 'question':
+                try:
+                    object_instance = Question.objects.get(id=object_id)
+                except ObjectDoesNotExist:
+                    return JsonResponse({'response': 'Invalid Question', 'type': 'comment'}, status=400)
+            if self.request.user.is_authenticated:
                 object_instance.comments.create(commenter=self.request.user,
                                                 content=comment_form.cleaned_data['content'])
                 return JsonResponse({'response': 'Comment created', 'type': 'comment'})
             else:
-                return JsonResponse({'response': 'Invalid Comment', 'type': 'comment'}, status=400)
+                url = reverse('account:login')
+                url = '{}?next={}'.format(url, reverse('questions:detail', args=[object_instance.slug]))
+                url = 'You have to be logged in to comment. Login/Signup <a href="{}">here</a>'.format(url)
+                return JsonResponse({'response': url, 'type': 'comment'})
         else:
-            url = reverse('account:login')
-            url = '{}?next={}'.format(url, reverse('questions:detail', args=[object_instance.slug]))
-            url = 'You have to be logged in to comment. Login/Signup <a href="{}">here</a>'.format(url)
-            return JsonResponse({'response': url, 'type': 'comment'})
+            return JsonResponse({'response': 'Invalid Comment', 'type': 'comment'}, status=400)
